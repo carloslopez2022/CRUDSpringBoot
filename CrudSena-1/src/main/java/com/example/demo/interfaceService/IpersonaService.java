585da7747f7d7a3a5ca7a6d9a4516216c5205f1d
carloslopez2022 <clubcorrecaminos2017@gmail.com>
2024-02-06 19:55:40 -0500

    se agrego codigo inical testeado
    se agrega todo el codigo referente al crud en spring boot probado

---
+++ CrudSena-1/src/main/java/com/example/demo/interfaceService/IpersonaService.java
@@ -0,0 +1,20 @@
+package com.example.demo.interfaceService;
+
+import java.util.List;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.modelo.Persona;
+
+
+
+@Service
+public interface IpersonaService {
+	public List<Persona>listar();
+	public Optional<Persona>listarId(int id);
+	public int save(Persona p);
+	public void delete(int id);
+
+}
